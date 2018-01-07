f551c8c9f46927b4e262478620bf4f52270d1b57
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-01-06 20:59:22 -0500

    pull out Todo

---
+++ src/components/Todo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-bind:class="{done: todo.done}">
+    <b>{{todo.title}}</b> ({{todo.project}})
+  </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+    props: ['todo']
+  }
+</script>
+
+<style scoped>
+div.done {
+  text-decoration: line-through;
+  color: #b0b0b0;
+}
+</style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,37 +1,22 @@
 <template>
   <div class="hello">
-    <ul>
-      <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
-      <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
-      <li v-for="todo in todos">
-        <div v-bind:class="{done: todo.done}">
-          <b>{{todo.title}}</b> ({{todo.project}})
-        </div>
-      </li>
-    </ul>
+    <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+    <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+    <Todo v-for="todo in todos" :todo="todo"></Todo>
   </div>
 </template>
 
 <script>
+import Todo from './Todo'
+
 export default {
+  components: {
+    Todo
+  },
   props: ['todos']
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  margin: 0 10px;
-}
-div.done {
-  text-decoration: line-through;
-  color: #b0b0b0;
-}
 </style>
